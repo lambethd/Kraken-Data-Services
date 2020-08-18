2e85ee01a4c42fd10e373907820c82cb85533556
David Lambeth <david.lambeth@outlook.com>
2020-08-18 22:52:58 +0100

    Event Controller + Graph Range stuff

--- src/main/java/lambethd/kraken/application/service/GraphService.java
+++ src/main/java/lambethd/kraken/application/service/GraphService.java
@@ -1,20 +1,36 @@
 package lambethd.kraken.application.service;
 
 import lambethd.kraken.application.interfaces.IGraphService;
+import lambethd.kraken.application.interfaces.IRangeService;
 import lambethd.kraken.data.mongo.repository.IGraphRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import portfolio.RangeType;
 import runescape.Graph;
 
-import java.util.List;
+import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 @Service
 public class GraphService implements IGraphService {
     @Autowired
     private IGraphRepository graphRepository;
+    @Autowired
+    private IRangeService rangeService;
 
     @Override
-    public Graph getGraphById(Integer id) {
+    public Graph getGraph(Integer id) {
         return graphRepository.getGraphById(id);
+    }
+
+    @Override
+    public Graph getGraph(Integer id, RangeType rangeType) {
+        Graph graph = graphRepository.getGraphById(id);
+        trimGraph(graph, rangeService.getDateFromRange(rangeType));
+        return graph;
+    }
+
+    private void trimGraph(Graph graph, LocalDateTime startDate) {
+        graph.daily = graph.daily.stream().filter(g -> g.getKey().isAfter(startDate)).collect(Collectors.toList());
     }
 }
